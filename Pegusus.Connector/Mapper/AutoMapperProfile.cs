28b31cd81e6b8198947e8f4f669f10ec6af795af
Kaushalya Harshanath <kaushalya.h@evicio.com>
2020-11-04 14:07:21 +0530

    fEATURE#0003-formating code

---
+++ Pegusus.Connector/Mapper/AutoMapperProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Pegusus.Data.Model;
+using Pegusus.Services.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pegusus.Connector.Mapper
+{
+    public class AutoMapperProfile:Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<User, UserViewModels>();
+        }
+     
+    }
+}
